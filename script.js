f46355db388e3f43fe1cc5551b90200f4c929cb9
oguzhanatlan <osi.atlan.19@gmail.com>
2024-07-19 21:02:40 +0300

    index.html changed

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+var _alex = [90,100,70];
+var _sara = [90,40,70];
+var _nancy = [100,60,70];
+
+var _alexOrtalama = (_alex[0] + _alex[1] + _alex[2]) / 3;
+var _saraOrtalama = (_sara[0] + _sara[1] + _sara[2]) / 3;
+var _nancyOrtalama = (_nancy[0] + _nancy[1] + _nancy[2]) / 3;
+
+
+alert(_alexOrtalama);
